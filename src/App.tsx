07284b31ff95626ad07ecbf94c660b78516da342
Jorge de Souza J Junior <jorgedesouza@recrutae.com.br>
2022-09-03 19:53:50 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <div className="App">
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyle/>
+
+      <h1> Hello world</h1>
+      </ThemeProvider>
+    </div>
+  );
+}
