88b4651675604e8784833992fe75fdff0c8fbbf7
Jorge de Souza <jorgejrdj1@gmail.com>
2022-09-29 17:03:22 -0300

    feat: create accessible radio component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import LogoIcon from "../../assets/Logo.icon";
+import { NewTransactionModal } from "../NewTransactionModal";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 export const Header = () => {
@@ -7,22 +8,15 @@ export const Header = () => {
     <HeaderContainer>
       <HeaderContent>
         <LogoIcon />
-        
+
         <Dialog.Root>
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay />
-
-            <Dialog.Content>
-              <Dialog.Title>Nova Transação</Dialog.Title>
-
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
+
       </HeaderContent>
     </HeaderContainer>
   );
