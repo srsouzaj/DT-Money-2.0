184267b8b4ac15176f99b31b80198059996cc15a
Jorge de Souza J Junior <jorgedesouza@recrutae.com.br>
2022-09-29 15:06:05 -0300

    feat: summary component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,10 +1,12 @@
 import { Header } from "../../components/Header"
+import { Summary } from "../../components/Summary"
 import { TransactionContainer } from "./styles"
 
 export const Transactions = () => {
     return(
         <TransactionContainer>
         <Header/>
+        <Summary />
         </TransactionContainer>
     )
 }
