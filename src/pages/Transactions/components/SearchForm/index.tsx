a56ffed61d056ff75e15e653d922d30fd25602f7
Jorge de Souza <jorgejrdj1@gmail.com>
2022-09-29 22:13:59 -0300

    feat: add json-server

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -4,12 +4,14 @@ import { SearchFormContainer } from "./styles";
 export function SearchForm() {
   return (
     <SearchFormContainer>
+       
       <input type="text" placeholder="Busque por transações" />
 
       <button type="submit">
         <MagnifyingGlass size={20} />
         Buscar
       </button>
+     
     </SearchFormContainer>
   );
 }
